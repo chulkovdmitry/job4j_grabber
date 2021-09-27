a58833ab7eca89d17bec21ed013206d5c2cdd53f
chulkovdmitry <dmitry_c2000@mail.ru>
2021-09-27 10:10:02 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -2,12 +2,10 @@ package ru.job4j.quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.Properties;
-
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
@@ -15,35 +13,66 @@ import static org.quartz.SimpleScheduleBuilder.*;
 public class AlertRabbit {
     public static void main(String[] args) {
         try {
+            Properties properties = setProperties();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule().withIntervalInSeconds(setProperties())
-                    .repeatForever();
-            Trigger trigger = newTrigger().startNow().withSchedule(times).build();
-            scheduler.scheduleJob(job, trigger);
+            try (Connection connection = initConnection(properties)) {
+                JobDataMap data = new JobDataMap();
+                data.put("connection", connection);
+                JobDetail job = newJob(Rabbit.class)
+                        .usingJobData(data)
+                        .build();
+                int interval = Integer.parseInt(properties.getProperty("rabbit.interval"));
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(interval)
+                        .repeatForever();
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                scheduler.scheduleJob(job, trigger);
+                Thread.sleep(10000);
+                scheduler.shutdown();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } catch (SchedulerException se) {
             se.printStackTrace();
         }
     }
 
-    private static int setProperties() {
-        int time = 0;
-        try (BufferedReader in = new BufferedReader(
-                new FileReader("src/main/resources/rabbit.properties"))) {
-            Properties properties = new Properties();
-            properties.load(in);
-            time = Integer.parseInt(properties.getProperty("rabbit.interval"));
-        } catch (IOException e) {
+    private static Properties setProperties() {
+        Properties properties = new Properties();
+        ClassLoader loader = JobDetail.class.getClassLoader();
+        try (InputStream io = loader.getResourceAsStream("rabbit.properties")) {
+            properties.load(io);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return time;
+        return properties;
+    }
+
+    private static Connection initConnection(Properties properties) throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
+        return DriverManager.getConnection(url, login, password);
     }
 
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
+            Connection connection = (Connection) context.getJobDetail()
+                    .getJobDataMap().get("connection");
+            try (PreparedStatement statement =
+                         connection.prepareStatement("insert into rabbit(created_date) values (?)")) {
+                statement.setDate(1, Date.valueOf(LocalDate.now()));
+                statement.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }

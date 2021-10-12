2f02850f1ea1af499680c6f9994aaa0c1ff36567
chulkovdmitry <dmitry_c2000@mail.ru>
2021-10-12 11:21:24 +0300

    2.3. Загрузка деталей поста. [#285212]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,87 @@
+package ru.job4j.grabber;
+
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+
+    private int id = Statement.RETURN_GENERATED_KEYS;
+
+    private String title;
+
+    private String link;
+
+    private String description;
+
+    private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(title, post.title) && Objects.equals(link, post.link)
+                && Objects.equals(created.withNano(0), post.created.withNano(0));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, link, created);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+}
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,20 +4,69 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Post;
+import ru.job4j.grabber.utils.Parse;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        for (int i = 1; i < 6; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+public class SqlRuParse implements Parse {
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> rsl = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(link).get();
             Elements row = doc.select(".postslisttopic");
-            System.out.println("******************** Страница " + i + " ********************");
-            for (Element td : row) {
-                Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                Element date = td.parent().child(5);
-                System.out.println(date.text());
+            for (Element ed : row) {
+                if (ed.text().contains("Важно")) {
+                    continue;
+                }
+                Post post = new Post();
+                post.setId(Statement.RETURN_GENERATED_KEYS);
+                System.out.println(post.getId());
+                post.setTitle(ed.child(0).text());
+
+                post.setLink(ed.child(0).attr("href"));
+
+                SqlRuDateTimeParser sql = new SqlRuDateTimeParser();
+                LocalDateTime date = sql.parse(ed.parent().children().get(5).text());
+                post.setCreated(date);
+                rsl.add(post);
             }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return rsl;
+    }
+
+    @Override
+
+    public Post detail(String link) {
+        return null;
+    }
+
+    public static String loadString(String url) throws Exception {
+        Document doc = Jsoup.connect(url).get();
+        Elements row = doc.select(".msgBody");
+        Element message = row.get(1);
+
+        Elements row1 = doc.select(".msgFooter");
+        Element date = row1.get(0);
+
+        String[] dateArray = date.text().split(", ");
+        String[] dateArray2 = dateArray[1].split(" ");
+        String day = dateArray[0] + ", " + dateArray2[0];
+
+        return message.text() + " ДАТА СООБЩЕНИЯ: " + day;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        String exp = loadString("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
+        System.out.println(exp);
     }
 }

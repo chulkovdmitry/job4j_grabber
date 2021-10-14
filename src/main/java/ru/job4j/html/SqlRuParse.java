f8061cce1d395ad77183bc58000125210d7c0805
chulkovdmitry <dmitry_c2000@mail.ru>
2021-10-14 09:31:36 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,6 +5,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.Parse;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 import java.sql.Statement;
@@ -14,26 +15,24 @@ import java.util.List;
 
 public class SqlRuParse implements Parse {
 
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
     @Override
     public List<Post> list(String link) {
         List<Post> rsl = new ArrayList<>();
         try {
             Document doc = Jsoup.connect(link).get();
             Elements row = doc.select(".postslisttopic");
-            for (Element ed : row) {
-                if (ed.text().contains("Важно")) {
+            for (Element el : row) {
+                if (el.text().contains("Важно")) {
                     continue;
                 }
-                Post post = new Post();
-                System.out.println(post.getId());
-                post.setTitle(ed.child(0).text());
-                post.setLink(ed.child(0).attr("href"));
-                SqlRuDateTimeParser sql = new SqlRuDateTimeParser();
-                LocalDateTime date = sql.parse(ed.parent().children().get(5).text());
-                post.setCreated(date);
-                rsl.add(post);
+                rsl.add(detail(el.children().attr("href")));
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -43,27 +42,42 @@ public class SqlRuParse implements Parse {
     @Override
 
     public Post detail(String link) {
-        return null;
+        Post post = new Post();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements row = doc.select(".messageHeader");
+            Element name = row.first();
+            post.setTitle(name.text());
+            post.setLink(link);
+            post.setDescription(loadString(link));
+            String[] date1 = name.parent().parent().children().get(2).text().split(", ");
+            String[] date2 = date1[1].split(" ");
+            String rsl = date1[0] + ", " + date2[0];
+            post.setCreated(dateTimeParser.parse(rsl));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
     }
 
     public static String loadString(String url) throws Exception {
         Document doc = Jsoup.connect(url).get();
         Elements row = doc.select(".msgBody");
         Element message = row.get(1);
-
         Elements row1 = doc.select(".msgFooter");
         Element date = row1.get(0);
-
         String[] dateArray = date.text().split(", ");
         String[] dateArray2 = dateArray[1].split(" ");
         String day = dateArray[0] + ", " + dateArray2[0];
-
         return message.text() + " ДАТА СООБЩЕНИЯ: " + day;
     }
 
 
     public static void main(String[] args) throws Exception {
-        String exp = loadString("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
-        System.out.println(exp);
+        DateTimeParser date = new SqlRuDateTimeParser();
+        SqlRuParse sql = new SqlRuParse(date);
+        List<Post> rsl = sql.list("https://www.sql.ru/forum/job-offers/1");
+        System.out.println(rsl.get(6).getCreated());
     }
 }

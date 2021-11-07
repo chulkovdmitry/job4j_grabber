ec293f7c78ed55182c0c9c42e2b6e1877ed0e939
chulkovdmitry <dmitry_c2000@mail.ru>
2021-11-07 12:42:26 +0300

    1. Реализация кеша на SoftReference [#1592 #223600] испр

--- src/main/java/ru/job4j/cache/Emulator.java
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -1,29 +1,48 @@
 package ru.job4j.cache;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Emulator {
-    private final DirFileCache dir;
 
-    public Emulator(String dirFileCache) {
-        dir = new DirFileCache(dirFileCache);
-    }
+    private static final String MENU_DELIMITER = "======";
+    private static final String MENU_FILE_NAME = "Enter file name:";
+    private static final String MENU_LOAD = "load";
+    private static final String MENU_GET = "get";
+    private static final String MENU_EXIT = "exit";
 
-    public void load(String key) {
-        dir.load(key);
-    }
-
-    public String getCacheData(String key) {
-        return dir.get(key);
-    }
-
-    public static void main(String[] args) {
-        Emulator emulator = new Emulator("src/main/java/ru/job4j/cache/");
-        emulator.load("Names.txt");
-        String data = emulator.getCacheData("Names.txt");
-        System.out.println("Names.txt :");
-        System.out.println(data);
-        emulator.load("Address.txt");
-        data = emulator.getCacheData("Address.txt");
-        System.out.println("Address.txt :");
-        System.out.println(data);
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter your directory path:");
+        Scanner in = new Scanner(System.in);
+        String dir = in.nextLine();
+        if (!new File(dir).isDirectory()) {
+            throw new IllegalArgumentException("Not a directory");
+        }
+        System.out.println(MENU_DELIMITER);
+        AbstractCache<String, String> cache = new DirFileCache(dir);
+        boolean isRunning = true;
+        System.out.printf("Enter \"%s\" to load file%n", MENU_LOAD);
+        System.out.printf("Enter \"%s\" to display file's content%n", MENU_GET);
+        System.out.printf("Enter \"%s\" to exit%n", MENU_EXIT);
+        System.out.println(MENU_DELIMITER);
+        while (isRunning) {
+            String input = in.nextLine();
+            if (input.equals(MENU_EXIT)) {
+                isRunning = false;
+            }
+            if (input.equals(MENU_LOAD)) {
+                System.out.println(MENU_FILE_NAME);
+                String fileName = in.nextLine();
+                cache.put(fileName, cache.load(fileName));
+                System.out.println(MENU_DELIMITER);
+            }
+            if (input.equals(MENU_GET)) {
+                System.out.println(MENU_FILE_NAME);
+                String value = cache.get(in.nextLine());
+                System.out.println(value != null ? value : "No content or no such file found");
+                System.out.println(MENU_DELIMITER);
+            }
+        }
     }
 }

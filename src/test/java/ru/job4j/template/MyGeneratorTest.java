724569306bc947e65e1e434c6b3c531fb4f916a7
chulkovdmitry <dmitry_c2000@mail.ru>
2021-11-08 15:46:35 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/MyGeneratorTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.template;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyGeneratorTest {
+
+    @Ignore
+    @Test
+    public void produce() {
+        Generator gen = new MyGenerator();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr");
+        args.put("subject", "you");
+        String rsl = gen.produce("I am a ${name}, Who are ${subject}? ", args);
+        Assert.assertEquals(rsl, "I am a Petr, Who are you? ");
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void produceWhenLessArgs() {
+        Generator gen = new MyGenerator();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr");
+        String rsl = gen.produce("I am a ${name}, Who are ${subject}? ", args);
+    }
+
+    @Ignore
+    @Test
+    public void produceWhenMoreArgs() {
+        Generator gen = new MyGenerator();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr");
+        args.put("subject", "you");
+        args.put("age", "28");
+        String rsl = gen.produce("I am a ${name}, Who are ${subject}? ", args);
+        Assert.assertEquals(rsl, "I am a Petr, Who are you? ");
+    }
+}

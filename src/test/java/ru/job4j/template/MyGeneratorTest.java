61047b278049f95f1b4756bab99e3de75dab14c0
chulkovdmitry <dmitry_c2000@mail.ru>
2021-11-13 11:23:07 +0300

    3. Шаблонизатор. [#855] испр. 2

--- src/test/java/ru/job4j/template/MyGeneratorTest.java
+++ src/test/java/ru/job4j/template/MyGeneratorTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class MyGeneratorTest {
 
     @Ignore
@@ -41,4 +43,14 @@ public class MyGeneratorTest {
         Assert.assertNotEquals(rsl, "I am a Pavel, Who are you? ");
     }
 
+    @Ignore
+    @Test
+    public void produceWhenWrongArgs() {
+        Generator gen = new MyGenerator();
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr");
+        args.put("subject", "you");
+        String rsl = gen.produce("I am a ${name}, Who are ${subj}? ", args);
+        Assert.assertEquals(rsl, "I am a Petr, Who are ${subj}? ");
+    }
 }

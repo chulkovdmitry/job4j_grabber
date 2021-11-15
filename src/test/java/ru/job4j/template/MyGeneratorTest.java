edc7b6a57ae9d07c780387184a20faaf3147ef78
chulkovdmitry <dmitry_c2000@mail.ru>
2021-11-15 18:07:11 +0300

    3. Шаблонизатор. [#855] испр. 3

--- src/test/java/ru/job4j/template/MyGeneratorTest.java
+++ src/test/java/ru/job4j/template/MyGeneratorTest.java
@@ -32,25 +32,11 @@ public class MyGeneratorTest {
     }
 
     @Ignore
-    @Test
-    public void produceWhenMoreArgs() {
-        Generator gen = new MyGenerator();
-        Map<String, String> args = new HashMap<>();
-        args.put("name", "Petr");
-        args.put("subject", "you");
-        args.put("age", "28");
-        String rsl = gen.produce("I am a ${name}, Who are ${subject}? ", args);
-        Assert.assertNotEquals(rsl, "I am a Pavel, Who are you? ");
-    }
-
-    @Ignore
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void produceWhenWrongArgs() {
         Generator gen = new MyGenerator();
         Map<String, String> args = new HashMap<>();
         args.put("name", "Petr");
-        args.put("subject", "you");
-        String rsl = gen.produce("I am a ${name}, Who are ${subj}? ", args);
-        Assert.assertEquals(rsl, "I am a Petr, Who are ${subj}? ");
+        gen.produce("${nam}", args);
     }
 }

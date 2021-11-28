faf0e7aa1e63c2e56461c8fbc3a8f0f35eb1b711
chulkovdmitry <dmitry_c2000@mail.ru>
2021-11-28 12:37:08 +0300

    1. Динамическое перераспределение продуктов [#854]

--- src/test/java/ru/job4j/ood/lsp/products/ControllQualityTest.java
+++ src/test/java/ru/job4j/ood/lsp/products/ControllQualityTest.java
@@ -59,4 +59,30 @@ public class ControllQualityTest {
                 Arrays.toString(new int[]{1, 2, 1}));
     }
 
+    @Test
+    public void whenResort() {
+        ControllQuality checker = new ControllQuality(List.of(
+                new Warehouse(),
+                new Shop(),
+                new Trash()
+        ));
+        LocalDate now = LocalDate.now();
+        Milk milk = new Milk("Milk", now.minusDays(4), 60, 0);
+        Bread bread = new Bread("Bread", now, 32, 0);
+        Milk anotherMilk = new Milk("Milk", now.minusDays(2), 60, 0);
+        Bread anotherBread = new Bread("Bread", now.minusDays(10), 32, 0);
+        checker.distribute(milk);
+        checker.distribute(bread);
+        checker.distribute(anotherMilk);
+        checker.distribute(anotherBread);
+        bread.setCreateDate(now.minusDays(4));
+        bread.setExpiryDate(now.minusDays(1));
+        checker.resort();
+        List<Storage> storages = checker.getStorages();
+        assertEquals(Arrays.toString(new int[]{
+                        storages.get(0).getList().size(),
+                        storages.get(1).getList().size(),
+                        storages.get(2).getList().size()}),
+                Arrays.toString(new int[]{0, 2, 3}));
+    }
 }

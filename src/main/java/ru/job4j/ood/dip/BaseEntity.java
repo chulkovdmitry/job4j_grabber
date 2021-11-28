2cd57fbedf0b235eb350eb8556c7a63d7044ff1b
chulkovdmitry <dmitry_c2000@mail.ru>
2021-11-28 12:21:43 +0300

    0. Принцип инверсии зависимостей [#4917] урок

---
+++ src/main/java/ru/job4j/ood/dip/BaseEntity.java
@@ -0,0 +1,52 @@
+package ru.job4j.ood.dip;
+
+import java.util.Objects;
+
+/**
+ * Базовая сущность
+ * @author chulkvdv
+ * @version 1.0
+ * @since 28.11.2021
+ */
+public abstract class BaseEntity {
+    protected int id;
+    protected String name;
+
+    public BaseEntity(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return id == that.id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
